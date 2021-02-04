f072ff90bf0a1c91c1e70749ce9ed08f023d9f08
lachlandk <lachlandufort-kennett@outlook.com>
2021-02-04 20:30:45 +0000

    ray-optics: added touch functionality

--- ray-optics/scripts/controls.js
+++ ray-optics/scripts/controls.js
@@ -1,20 +1,88 @@
-function mouseDown(event){
+function mouseDown(event) {
+  pointerDown(event, 100, function(){window.addEventListener("mousemove", mouseDrag);});
+  canvas.event_canvas.removeEventListener("mousedown", mouseDown);
+	window.addEventListener("mouseup", mouseUp);
+}
+
+function touchStart(event) {
+  const touches = event.changedTouches;
+  event.preventDefault();
+  for (let i = 0; i < touches.length; i++) {
+    ongoingTouches.push({identifier: touches[i].identifier, clientX: touches[i].clientX, clientY: touches[i].clientY});
+    pointerDown(touches[i], 1000, function(){window.addEventListener("touchmove", touchDrag, {passive: false});}); // addEventListener not needed here, event listener can be active all the time
+    canvas.event_canvas.removeEventListener("touchstart", touchStart);
+    window.addEventListener("touchend", touchEnd);
+  }
+}
+
+function mouseDrag(event) {
+  pointerDrag(event);
+}
+
+function touchDrag(event) {
+  const touches = event.changedTouches;
+  event.preventDefault();
+  for (let i = 0; i < touches.length; i++) {
+    const index = ongoingTouchIndexById(touches[i].identifier);
+    if (index >= 0) {
+      pointerDrag(touches[i]);
+      ongoingTouches.splice(index, 1, {identifier: touches[i].identifier, clientX: touches[i].clientX, clientY: touches[i].clientY});
+    }
+  }
+}
+
+function mouseUp(event) {
+  canvas.event_canvas.addEventListener("mousedown", mouseDown);
+	window.removeEventListener("mouseup", mouseUp);
+  window.removeEventListener("mousemove", mouseDrag);
+  pointerUp();
+  mouseOver(event);
+}
+
+function touchEnd(event) {
+  const touches = event.changedTouches;
+  event.preventDefault();
+  for (let i = 0; i < touches.length; i++) {
+    const index = ongoingTouchIndexById(touches[i].identifier);
+    if (index >= 0) {
+      canvas.event_canvas.addEventListener("touchstart", touchStart, {passive: false});
+      window.removeEventListener("touchend", touchEnd);
+      window.removeEventListener("touchmove", touchDrag);
+      pointerUp();
+      ongoingTouches.splice(index, 1);
+    }
+  }
+}
+
+function touchCancel(event) {
+  const touches = event.changedTouches;
+  event.preventDefault();
+  for (let i = 0; i < touches.length; i++) {
+    const index = ongoingTouchIndexById(touches[i].identifier);
+    if (index >= 0) {
+      ongoingTouches.splice(index, 1);
+    }
+  }
+}
+
+
+function pointerDown(pointer, hitRadius, setDragEventListener) {
 	// this function handles the mouseDown event in its entirety
 	const boundingRect = canvas.event_canvas.getBoundingClientRect(),
-		mouseX = (event.clientX - boundingRect.left) * (canvas.width / boundingRect.width) - (canvas.width / 2),
-		mouseY = (event.clientY - boundingRect.top) * (canvas.height / boundingRect.height) - (canvas.height / 2),
+		mouseX = (pointer.clientX - boundingRect.left) * (canvas.width / boundingRect.width) - (canvas.width / 2),
+		mouseY = (pointer.clientY - boundingRect.top) * (canvas.height / boundingRect.height) - (canvas.height / 2),
 		objectX = canvas.object.x * canvas.grid_scale,
 		objectY = canvas.object.y * canvas.grid_scale,
 		leftFocalPoint = (canvas.focalLength > 0 ? -canvas.focalLength : canvas.focalLength) * canvas.grid_scale,
 		rightFocalPoint = -leftFocalPoint;
 
-	function hitDetect(pointX, pointY, type){
+	function hitDetect(pointX, pointY, type) {
 		// if the clicked point is in range of point of interest, set corresponding dragging property to true
-		if (function(){
+		if (function() {
 			const dx = mouseX - pointX,
 				dy = mouseY - pointY;
-			return ((dx**2 + dy**2) <= 100);
-		}()){
+			return ((dx**2 + dy**2) <= hitRadius);
+		}()) {
 			heldX = mouseX - pointX;
 			heldY = mouseY - pointY;
 			switch (type) {
@@ -29,7 +97,7 @@ function mouseDown(event){
 					break;
 			}
 			canvas.event_canvas.style.cursor = "grabbing";
-			window.addEventListener("mousemove", mouseDrag);
+      setDragEventListener();
 		}
 	}
 
@@ -37,30 +105,27 @@ function mouseDown(event){
 	hitDetect(objectX, objectY, "object");
 	hitDetect(leftFocalPoint, 0, "leftFocalPoint");
 	hitDetect(rightFocalPoint, 0, "rightFocalPoint");
-
-	canvas.event_canvas.removeEventListener("mousedown", mouseDown);
-	window.addEventListener("mouseup", mouseUp);
 }
 
 // TODO: touch functionality, make handles bigger for ease of use on touch devices
 
-function mouseDrag(event){
+function pointerDrag(pointer) {
 	// this function runs when the mouse is moved while holding down LMB
 	const boundingRect = canvas.event_canvas.getBoundingClientRect(),
-		mouseX = (event.clientX - boundingRect.left) * (canvas.width / boundingRect.width) - (canvas.width / 2),
-		mouseY = (event.clientY - boundingRect.top) * (canvas.height / boundingRect.height) - (canvas.height / 2);
+		mouseX = (pointer.clientX - boundingRect.left) * (canvas.width / boundingRect.width) - (canvas.width / 2),
+		mouseY = (pointer.clientY - boundingRect.top) * (canvas.height / boundingRect.height) - (canvas.height / 2);
 	let newX = mouseX - heldX,
 		newY = mouseY - heldY;
 	const gridX = Math.round(newX / canvas.grid_size) * canvas.grid_size,
 		gridY = Math.round(newY / canvas.grid_size) * canvas.grid_size;
 
-	if (canvas.objectDragging){
+	if (canvas.objectDragging) {
 		const minX = -(canvas.width / 2) + 5,
 			maxX = 0,
 			maxY = -(canvas.height / 2) + 25,
 			minY = (canvas.height / 2) - 25;
 
-		if ((newX % canvas.grid_size > -10 || newX % canvas.grid_size < -canvas.grid_size + 10) && (Math.abs(newY) % canvas.grid_size < 10 || Math.abs(newY) % canvas.grid_size > canvas.grid_size - 10)){
+		if ((newX % canvas.grid_size > -10 || newX % canvas.grid_size < -canvas.grid_size + 10) && (Math.abs(newY) % canvas.grid_size < 10 || Math.abs(newY) % canvas.grid_size > canvas.grid_size - 10)) {
 			newX = gridX;
 			newY = gridY;
 		}
@@ -71,11 +136,11 @@ function mouseDrag(event){
 		canvas.eventUpdate();
 	}
 
-	if (canvas.leftFocalPointDragging){
+	if (canvas.leftFocalPointDragging) {
 		const minX = -(canvas.width / 2) + 5,
 			maxX = 0;
 
-		if (newX % canvas.grid_size > -10 || newX % canvas.grid_size < -canvas.grid_size + 10){
+		if (newX % canvas.grid_size > -10 || newX % canvas.grid_size < -canvas.grid_size + 10) {
 			newX = gridX;
 		}
 		newX = (newX < minX) ? minX : ((newX > maxX) ? maxX : newX);
@@ -94,11 +159,11 @@ function mouseDrag(event){
 		canvas.eventUpdate();
 	}
 
-	if (canvas.rightFocalPointDragging){
+	if (canvas.rightFocalPointDragging) {
 		const minX = 0,
 			maxX = (canvas.width / 2) - 5;
 
-		if (newX % canvas.grid_size < 10 || newX % canvas.grid_size > canvas.grid_size - 10){
+		if (newX % canvas.grid_size < 10 || newX % canvas.grid_size > canvas.grid_size - 10) {
 			newX = gridX;
 		}
 		newX = (newX < minX) ? minX : ((newX > maxX) ? maxX : newX);
@@ -118,18 +183,14 @@ function mouseDrag(event){
 	}
 }
 
-function mouseUp(event){
+function pointerUp(pointer) {
 	// cancel the dragging event when no longer holding LMB
-	canvas.event_canvas.addEventListener("mousedown", mouseDown);
-	window.removeEventListener("mouseup", mouseUp);
 	canvas.objectDragging = false;
 	canvas.leftFocalPointDragging = false;
 	canvas.rightFocalPointDragging = false;
-	window.removeEventListener("mousemove", mouseDrag);
-	mouseOver(event);
 }
 
-function mouseOver(event){
+function mouseOver(event) {
 	// detect if mouse is hovering over a draggable point to change cursor
 	const boundingRect = canvas.event_canvas.getBoundingClientRect(),
 		mouseX = (event.clientX - boundingRect.left) * (canvas.width / boundingRect.width) - (canvas.width / 2),
@@ -138,7 +199,7 @@ function mouseOver(event){
 		objectY = canvas.object.y * canvas.grid_scale,
 		focalLength = canvas.focalLength * canvas.grid_scale;
 
-	function hasMouseOver(pointX, pointY){
+	function hasMouseOver(pointX, pointY) {
 		const dx = mouseX - pointX,
 			dy = mouseY - pointY;
 		return ((dx**2 + dy**2) <= 100);
@@ -155,23 +216,23 @@ function mouseOver(event){
 	}
 }
 
-function updateSurfaceType(){
+function updateSurfaceType() {
 	canvas.surfaceType = document.getElementById("surface-type-control").value;
 	canvas.drawBackground();
 	switch (canvas.surfaceType) {
 		case "convex-lens":
 		case "concave-mirror":
-			if (!isFinite(canvas.focalLength)){
+			if (!isFinite(canvas.focalLength)) {
 				canvas.focalLength = 200;
-			} else if (canvas.focalLength < 0){
+			} else if (canvas.focalLength < 0) {
 				canvas.focalLength *= -1;
 			}
 			break;
 		case "concave-lens":
 		case "convex-mirror":
-			if (!isFinite(canvas.focalLength)){
+			if (!isFinite(canvas.focalLength)) {
 				canvas.focalLength = -200;
-			} else if (canvas.focalLength > 0){
+			} else if (canvas.focalLength > 0) {
 				canvas.focalLength *= -1;
 			}
 			break;
@@ -182,12 +243,24 @@ function updateSurfaceType(){
 	canvas.eventUpdate();
 }
 
-const canvas = new Canvas()
+function ongoingTouchIndexById(id) {
+  for (let i = 0; i < ongoingTouches.length; i++) {
+    if (ongoingTouches[i].identifier == id) {
+      return  i;
+    }
+  }
+  return -1;
+}
+
+const canvas = new Canvas(),
+  ongoingTouches = [];
 let heldX,
 	heldY;
 canvas.event_canvas.addEventListener("mousedown", mouseDown);
+canvas.event_canvas.addEventListener("touchstart", touchStart, {passive: false});
 canvas.event_canvas.addEventListener("mousemove", mouseOver);
 canvas.event_canvas.addEventListener("contextmenu", function(event){event.preventDefault();});
+window.addEventListener("touchcancel", touchCancel, {passive: false});
 window.addEventListener("resize", function() {
 	canvas.setWidth(canvas.container.clientWidth);
 	canvas.setHeight(canvas.container.clientHeight);
