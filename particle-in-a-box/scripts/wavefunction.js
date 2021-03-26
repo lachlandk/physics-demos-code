6ab9cec323533b561d884d663dfb52529d3b9aab
lachlandk <lachlandufort-kennett@outlook.com>
2021-03-26 20:23:00 +0000

    particle-in-a-box: created main.html, main.css, wavefunction.js

---
+++ particle-in-a-box/scripts/wavefunction.js
@@ -0,0 +1,4 @@
+// https://www.st-andrews.ac.uk/physics/quvis/simulations_html5/sims/finite-infinite-well/finite-infinite-well.html
+// https://www.st-andrews.ac.uk/physics/quvis/simulations_html5/sims/infwell1d/infwell1d.html
+// http://physics.weber.edu/schroeder/software/SquareWell.html
+// https://phys.libretexts.org/Bookshelves/Modern_Physics/Book%3A_Spiral_Modern_Physics_%28D%27Alessandris%29/6%3A_The_Schr%C3%B6dinger_Equation/6.2%3A_Solving_the_1D_Infinite_Square_Well
